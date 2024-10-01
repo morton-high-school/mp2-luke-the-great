15bfcce5aa46ab501e8cde6ff231094e3e8e047f
luke-the-great <luke.baker@mcusd709.org>
2024-09-30 21:18:45 -0500

    donezees

--- ChatBot.java
+++ ChatBot.java
@@ -47,6 +47,14 @@ public class ChatBot{
       //statement.indexOf("Fortnite")
       //statement.indexOf("fortnite")
       response = "Best game OF ALL TIME!!!!";
+    }else if(findKeyword(statement, "I want to", 0)>-1){
+      response = transformIWantToStatement(statement);
+    }else if(findKeyword(statement, "you", 0)>-1 && findKeyword(statement, "me", 3)>0){
+      response = transformYouMeStatement(statement);
+    }else if(findKeyword(statement, "I want", 0)>-1){
+      response = transformIWantStatement(statement);
+    }else if(findKeyword(statement, "I", 0)>-1 && findKeyword(statement, "you", 1)>0){
+      response = transformIYouStatement(statement);
     }else{
       response = getRandomResponse();
     }
@@ -178,5 +186,32 @@ public class ChatBot{
 
 		String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe).trim();
 		return "What makes you think that I " + restOfStatement + " you?";
+  }
+
+    private String transformIWantStatement(String statement){
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals(".")){
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		int psn = findKeyword(statement, "I want", 0);
+		String restOfStatement = statement.substring(psn + 6).trim();
+		return "Would you really be happy if you had " + restOfStatement + "?";
 	}
+
+    private String transformIYouStatement(String statement){
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals(".")){
+			statement = statement.substring(0, statement.length() - 1);
+		}
+
+		int psnOfYou = findKeyword (statement, "I", 0);
+		int psnOfMe = findKeyword (statement, "you", psnOfYou + 1);
+
+		String restOfStatement = statement.substring(psnOfYou + 1, psnOfMe).trim();
+		return "Why do you " + restOfStatement + " me?";
+  }
 }
